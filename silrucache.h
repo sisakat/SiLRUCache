5d3b8534af722d2af828e86b6c6dd07dd3c55c8a
Stefan Isak <isakhandy@gmail.com>
2023-11-25 15:33:28 +0100

    Basic setup

---
+++ silrucache.h
@@ -0,0 +1,16 @@
+#ifndef SILRUCACHE_H
+#define SILRUCACHE_H
+
+template <typename T>
+class SiLRUCacheItem {
+public:
+private:
+};
+
+template <typename T>
+class SiLRUCache {
+public:
+private:
+};
+
+#endif
---
+++ test.cpp
@@ -0,0 +1,2 @@
+#include <catch2/catch_amalgamated.hpp>
+#include "silrucache.h"
